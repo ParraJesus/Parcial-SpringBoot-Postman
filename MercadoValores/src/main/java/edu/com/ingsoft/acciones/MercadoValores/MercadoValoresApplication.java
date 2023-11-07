34d765899f5a153c3d692dc67ebdd3fd0923f7e8
ParraJesus <jgparra@unicauca.edu.co>
2023-11-07 10:27:36 -0500

    inicialización

---
+++ MercadoValores/src/main/java/edu/com/ingsoft/acciones/MercadoValores/MercadoValoresApplication.java
@@ -0,0 +1,13 @@
+package edu.com.ingsoft.acciones.MercadoValores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoValoresApplication.class, args);
+	}
+
+}
